a7d6b30e64a71eede830811a9af70c392118371f
Joshua Weidanz <joshua@weidanz.de>
2023-08-20 18:13:52 +0200

    added timestamps + removed unused requirement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import asyncio
+from datetime import datetime
 from dotenv import load_dotenv
 import os
 
@@ -22,11 +23,13 @@ async def check_punishment_stats():
                 staff_total = data['staff_total']
                 watchdog_total = data['watchdog_total']
 
+                current_time = datetime.now().strftime('%H:%M:%S')
+
                 if last_staff_total != 0 and staff_total > last_staff_total:
-                    print(f"Staff ban #{staff_total}. This was a staff ban. Be careful!")
+                    print(f"Staff ban #{staff_total} ({current_time}). This was a staff ban. Be careful!")
 
                 if last_watchdog_total != 0 and watchdog_total > last_watchdog_total:
-                    print(f"Watchdog ban #{watchdog_total}. Don't worry, this was an anticheat ban.")
+                    print(f"Watchdog ban #{watchdog_total} ({current_time}). Don't worry, this was an anticheat ban.")
 
                 last_staff_total = staff_total
                 last_watchdog_total = watchdog_total
@@ -37,14 +40,16 @@ async def check_punishment_stats():
         await asyncio.sleep(1)  # Check every second
 
 banner = """
-  _    _                _             _   ____                  _   _         _    _  __   _             
- | |  | |              (_)           | | |  _ \                | \ | |       | |  (_)/ _| (_)            
- | |__| | _   _  _ __   _ __  __ ___ | | | |_) |  __ _  _ __   |  \| |  ___  | |_  _ | |_  _   ___  _ __ 
- |  __  || | | || '_ \ | |\ \/ // _ \| | |  _ <  / _` || '_ \  | . ` | / _ \ | __|| ||  _|| | / _ \| '__|
- | |  | || |_| || |_) || | >  <|  __/| | | |_) || (_| || | | | | |\  || (_) || |_ | || |  | ||  __/| |   
- |_|  |_| \__, || .__/ |_|/_/\_\\___| |_| |____/  \__,_||_| |_| |_| \_| \___/  \__||_||_|  |_| \___||_|   
-           __/ || |                                                                                      
-          |___/ |_|  
+
+  _    _             _          _   ____                _   _       _   _  __ _            __      __  __   ___  
+ | |  | |           (_)        | | |  _ \              | \ | |     | | (_)/ _(_)           \ \    / / /_ | |__ \ 
+ | |__| |_   _ _ __  ___  _____| | | |_) | __ _ _ __   |  \| | ___ | |_ _| |_ _  ___ _ __   \ \  / /   | |    ) |
+ |  __  | | | | '_ \| \ \/ / _ \ | |  _ < / _` | '_ \  | . ` |/ _ \| __| |  _| |/ _ \ '__|   \ \/ /    | |   / / 
+ | |  | | |_| | |_) | |>  <  __/ | | |_) | (_| | | | | | |\  | (_) | |_| | | | |  __/ |       \  /     | |_ / /_ 
+ |_|  |_|\__, | .__/|_/_/\_\___|_| |____/ \__,_|_| |_| |_| \_|\___/ \__|_|_| |_|\___|_|        \/      |_(_)____|
+          __/ | |                                                                                                
+         |___/|_|                                                                                                
+
 
 by joshiy13
 """
