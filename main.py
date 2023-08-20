0bfff315584549484df6f8c421e2497a07102b3b
Joshua Weidanz <joshua@weidanz.de>
2023-08-20 19:30:15 +0200

    added checking for new releases

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import asyncio
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+import re
+import webbrowser
 
 load_dotenv()
 
@@ -10,9 +12,11 @@ API_KEY = os.getenv("API_KEY")
 
 last_staff_total = 0
 last_watchdog_total = 0
+current_version = "1.3.0"
+new_version_displayed = False
 
 async def check_punishment_stats():
-    global last_staff_total, last_watchdog_total
+    global last_staff_total, last_watchdog_total, new_version_displayed
 
     while True:
         try:
@@ -34,26 +38,87 @@ async def check_punishment_stats():
                 last_staff_total = staff_total
                 last_watchdog_total = watchdog_total
 
+                
+                new_release = check_new_release()
+                if new_release:
+                    if not new_version_displayed:
+                        new_version_displayed = True
+                        print(updateBanner)
+                        return
+                    else:
+                        break
+
         except Exception as e:
             print(f'Error occurred while checking punishment stats: {e}')
 
-        await asyncio.sleep(1)  # Check every second
+        await asyncio.sleep(1)
 
-banner = """
+def check_new_release():
+    try:
+        releases_response = requests.get('https://api.github.com/repos/Joshiy13/Ban-Notifier/releases')
+        releases_data = releases_response.json()
 
-  _    _             _          _   ____                _   _       _   _  __ _            __      __  __   ___  
- | |  | |           (_)        | | |  _ \              | \ | |     | | (_)/ _(_)           \ \    / / /_ | |__ \ 
- | |__| |_   _ _ __  ___  _____| | | |_) | __ _ _ __   |  \| | ___ | |_ _| |_ _  ___ _ __   \ \  / /   | |    ) |
- |  __  | | | | '_ \| \ \/ / _ \ | |  _ < / _` | '_ \  | . ` |/ _ \| __| |  _| |/ _ \ '__|   \ \/ /    | |   / / 
- | |  | | |_| | |_) | |>  <  __/ | | |_) | (_| | | | | | |\  | (_) | |_| | | | |  __/ |       \  /     | |_ / /_ 
- |_|  |_|\__, | .__/|_/_/\_\___|_| |____/ \__,_|_| |_| |_| \_|\___/ \__|_|_| |_|\___|_|        \/      |_(_)____|
-          __/ | |                                                                                                
-         |___/|_|                                                                                                
+        if releases_data and len(releases_data) > 0:
+            latest_release = releases_data[0]
+            latest_release_tag = latest_release['tag_name']
+            latest_release_url = latest_release['html_url']
 
+            if compare_versions(latest_release_tag, current_version) > 0:
+                return latest_release_url
+
+    except Exception as e:
+        print(f'Error occurred while checking for new release: {e}')
+
+    return None
+
+def compare_versions(version1, version2):
+    pattern = r'(\d+).(\d+).(\d+)'
+    v1_match = re.match(pattern, version1)
+    v2_match = re.match(pattern, version2)
+
+    if v1_match and v2_match:
+        v1_major, v1_minor, v1_patch = map(int, v1_match.groups())
+        v2_major, v2_minor, v2_patch = map(int, v2_match.groups())
+
+        if v1_major != v2_major:
+            return v1_major - v2_major
+        elif v1_minor != v2_minor:
+            return v1_minor - v2_minor
+        else:
+            return v1_patch - v2_patch
+
+    return 0
+
+new_release_url = check_new_release()
+if new_release_url:
+    updateBanner = """
+
+  _   _                __      __           _                                _ _       _     _      _ 
+ | \ | |               \ \    / /          (_)                              (_) |     | |   | |    | |
+ |  \| | _____      __  \ \  / /__ _ __ ___ _  ___  _ __     __ ___   ____ _ _| | __ _| |__ | | ___| |
+ | . ` |/ _ \ \ /\ / /   \ \/ / _ \ '__/ __| |/ _ \| '_ \   / _` \ \ / / _` | | |/ _` | '_ \| |/ _ \ |
+ | |\  |  __/\ V  V /     \  /  __/ |  \__ \ | (_) | | | | | (_| |\ V / (_| | | | (_| | |_) | |  __/_|
+ |_| \_|\___| \_/\_/       \/ \___|_|  |___/_|\___/|_| |_|  \__,_| \_/ \__,_|_|_|\__,_|_.__/|_|\___(_)
+                                                                                                      
+                                                                                                      
+"""
+    print(updateBanner)
+    webbrowser.open_new_tab(new_release_url)
+    input("""
+Press enter to exit...""")
+else:
+    banner = """
+
+  _    _             _          _   ____                _   _       _   _  __ _              ____   ____   _____  
+ | |  | |           (_)        | | |  _ \              | \ | |     | | (_)/ _(_)            / /_ | |___ \ / _ \ \ 
+ | |__| |_   _ _ __  ___  _____| | | |_) | __ _ _ __   |  \| | ___ | |_ _| |_ _  ___ _ __  | | | |   __) | | | | |
+ |  __  | | | | '_ \| \ \/ / _ \ | |  _ < / _` | '_ \  | . ` |/ _ \| __| |  _| |/ _ \ '__| | | | |  |__ <| | | | |
+ | |  | | |_| | |_) | |>  <  __/ | | |_) | (_| | | | | | |\  | (_) | |_| | | | |  __/ |    | | | |_ ___) | |_| | |
+ |_|  |_|\__, | .__/|_/_/\_\___|_| |____/ \__,_|_| |_| |_| \_|\___/ \__|_|_| |_|\___|_|    | | |_(_)____(_)___/| |
+          __/ | |                                                                           \_\               /_/ 
+         |___/|_|                                                                                                 
 
 by joshiy13
-"""
-
-print(banner)
-
-asyncio.run(check_punishment_stats())
+    """
+    print(banner)
+    asyncio.run(check_punishment_stats())
